ccf785dd02c673f609a2f0615c37b09ece16c4cb
sergey s <som-ana@yandex.ru>
2023-04-10 21:56:57 +0300

    initial1 commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int price){
+        int milesToRub = 20;
+        int result = (price/milesToRub);
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        /*
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+        */
+
+        System.out.print("1000 руб  ");
+        System.out.print(service.calculate(1000));
+        System.out.println(" бонусов");
+        System.out.println();
+
+        System.out.println("500 руб");
+        System.out.print(service.calculate(500));
+        System.out.println(" бонусов");
+        System.out.println();
+
+        System.out.println("4480 руб");
+        System.out.print(service.calculate(4480));
+        System.out.println(" бонусов");
+    }
+}
